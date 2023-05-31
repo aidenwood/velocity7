5ff49432a3e5bbbfc2fad63c9783ecaa494ad096
Aiden Wood <aidenthomasgodbywood@gmail.com>
2023-05-31 10:28:53 +1000

    AlpineJS - npx add astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import alpinejs from "@astrojs/alpinejs";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx(), sitemap(), alpinejs()]
 });
