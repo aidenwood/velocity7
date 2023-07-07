48842c2c19a5ee93824c1e340831751fefcaaff5
Aiden Wood <aidenthomasgodbywood@gmail.com>
2023-07-07 14:14:12 +1000

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["inconsolata", "InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+        sans: ["Inconsolata", "InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
