1c4bbd2247c5bd22a65afab1981bd6ab3f1822ec
Aiden Wood <aidenthomasgodbywood@gmail.com>
2023-08-28 13:57:25 +1000

    install partytown with no use

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,19 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import alpinejs from "@astrojs/alpinejs";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aidxn.com',
-  integrations: [tailwind(
-    {
-      config: {
-        applyBaseStyles: false,
-      },
-    } [sitemap()],
-  ), image({
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }[sitemap()]), image({
     serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx(), sitemap(), alpinejs()],
+  }), mdx(), sitemap(), alpinejs(), partytown()],
   experimental: {
-		viewTransitions: true
-	}
+    viewTransitions: true
+  }
 });
