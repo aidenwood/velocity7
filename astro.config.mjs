36d0cdc6424f30e82b362d684068eb206c3ae0d6
Aiden Wood <aidenthomasgodbywood@gmail.com>
2023-09-04 15:06:57 +1000

    Revert "Revert "pew pew integrations""
    This reverts commit 6a1f8601cc07f5868d29111675f2b45236b5ba76.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import alpinejs from "@astrojs/alpinejs";
-
 import partytown from "@astrojs/partytown";
+import compress from "astro-compress";
+import purgecss from "astro-purgecss";
+
+import criticalCss from "astro-critical-css";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,7 +19,7 @@ export default defineConfig({
     }
   }[sitemap()]), image({
     serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx(), sitemap(), alpinejs(), partytown()],
+  }), mdx(), sitemap(), alpinejs(), partytown(), compress(), purgecss(), criticalCss()],
   experimental: {
     viewTransitions: true
   }
